80387728692d3ff9d0bf83ee2dc5c8e8b4f9f634
Egor-glitch-glitch <egorgorshkov33@gmail.com>
2026-02-01 13:29:58 +0300

    Финальный коммит

--- Surname_LR3_project.py
+++ Surname_LR3_project.py
@@ -28,3 +28,30 @@ def divide():
         print(f"Результат деления ({a} / {b}) = {result}")
     else:
         print("Ошибка: Деление на ноль невозможно!")
+def main():
+    while True:
+        print("\n--- Меню (Вариант 3 - Python) ---")
+        print("1. Ввести А")
+        print("2. Ввести В")
+        print("3. Выполнить операцию '*' (A * B)")
+        print("4. Выполнить операцию '/' (A / B)")
+        print("0. Выход")
+        
+        choice = input("Выберите пункт меню: ")
+        
+        if choice == '1':
+            input_a()
+        elif choice == '2':
+            input_b()
+        elif choice == '3':
+            multiply()
+        elif choice == '4':
+            divide()
+        elif choice == '0':
+            print("Выход из программы.")
+            break
+        else:
+            print("Неверный выбор, попробуйте снова.")
+
+if __name__ == "__main__":
+    main()
